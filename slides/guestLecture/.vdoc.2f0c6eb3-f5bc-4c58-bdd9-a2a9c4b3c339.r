999a6381043d1fc3bbf078cb12fe03972f435269
sammo3182 <sammo3182@sina.com>
2026-05-12 20:45:05 +0800

    pku talk slides

---
+++ slides/guestLecture/.vdoc.2f0c6eb3-f5bc-4c58-bdd9-a2a9c4b3c339.r
@@ -0,0 +1,20 @@
+library(pacman)
+
+p_load(
+  "icons"
+  #   "tidyverse",
+  #   "drhutools"
+)
+
+# Functions preload
+# set.seed(313)
+
+# theme_set(
+#   theme_minimal(base_size = 18)
+# )
+
+# theme_update(
+#   plot.title = element_text(size = 18),
+#   axis.title = element_text(size = 22),
+#   axis.text = element_text(size = 18)
+# )
